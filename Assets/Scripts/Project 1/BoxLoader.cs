024f5d5856a2b2d2ff5677bc303423f621a2859f
Robert - SydneySAE_lab201 <1035695@student.sae.edu.au>
2026-04-15 16:09:49 +1000

    catch up

--- Assets/Scripts/Project 1/BoxLoader.cs
+++ Assets/Scripts/Project 1/BoxLoader.cs
@@ -14,7 +14,9 @@ public class BoxLoader : MonoBehaviour
     public float waitTime = 1f;
 
     RaycastHit hit;
+    RaycastHit[] hits = new RaycastHit[10];
     float maxDistance = 4f;
+
 
     private void Start()
     {
@@ -32,7 +34,9 @@ public class BoxLoader : MonoBehaviour
 
     public void BoxDetected()
     {
-        
+        //int count = Physics.RaycastNonAlloc(transform.position, transform.forward, hits, maxDistance)
+
+
 
         if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
         {
@@ -49,13 +53,6 @@ public class BoxLoader : MonoBehaviour
 
         }
     }
-    //void FixedUpdate()
-    //{
-    //    if (isWaiting == false)
-    //    {
-    //        StartCoroutine(SpawnBoxObjectFromPool());
-    //    }
-    //}
 
     public IEnumerator SpawnBoxObjectFromPool()
     {
@@ -77,33 +74,5 @@ public class BoxLoader : MonoBehaviour
         // isWaiting = false;
     }
 }
-//old Loading
-//public Storage package = new Storage();
-
-////if box is on top compare to spawn same objects
-
-//void Start()
-//{
-//    StartCoroutine(LoadObjectsFromJson());
-//}
-
-//IEnumerator LoadObjectsFromJson()
-//{
-//    string filePath = Path.Combine(Application.streamingAssetsPath, "StorageData.json");
-//    string jsonData = File.ReadAllText(filePath);
-//    package = JsonUtility.FromJson<Storage>(jsonData);
-
-//    foreach (Parcels item in package.itemsToDeliver)
-//    {
-//        GameObject prefab = Resources.Load<GameObject>("Prefabs/" + item.boxName);
-//        GameObject boxPrefab = Instantiate(prefab, new Vector3(0, 1.5f, 0), Quaternion.identity);
-
-//        yield return new WaitForSeconds(waitTime);
-//    }
-//    yield return null;
-
-//    //need to find a way to make the boxes load not at same time could do that with async from week3, but will need to discuss this with the teacher.
-//}
-
 
 
